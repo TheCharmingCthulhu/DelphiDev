6821a468ef2010ba27bcd6296546f3f5c5fe3a62
David Dembinski <darkvplayer@gmail.com>
2017-04-07 13:38:13 +0200

    Delphi AHK v0.0.0.2:   + Implementations:     + Execute(String) // Execute script     + Variables[Name: String] // Retrieves a variable     + ThreadReady // Used to check if thread is ready...   + Changes:     + FThread // Renamed & used as a starting thread always for execute to exec scripts...

--- Console/AHK/Source/AHKManager.pas
+++ Console/AHK/Source/AHKManager.pas
@@ -6,17 +6,27 @@ uses
   SysUtils, Variants, Classes, Generics.Collections, Winapi.Windows;
 
 type
-  TAHKTextDll = procedure(AScript: PWideString; AOptions: PWideString; AParameters: PWideString); cdecl;
+  TAHKThread = function(AScript: PWideString; AOptions: PWideString; AParameters: PWideString): THandle; cdecl;
+  TAHKThreadReady = function: Integer; cdecl;
+  TAHKExecute = function(AScript: PWideString): Boolean; cdecl;
+  TAHKGetVar = function(AVariableName: PWideString; AType: LongWord): PWideString; cdecl;
 
   TAHKManager = class
   private
     FHandle: THandle;
-    FTextDll: TAHKTextDll;
+    FThread: TAHKThread;
+    FThreadReady: TAHKThreadReady;
+    FExecute: TAHKExecute;
+    FGetVariable: TAHKGetVar;
+    function GetVariable(Name: String): String;
+    procedure SetVariable(Name: String; const Value: String);
   public
     constructor Create;
-    procedure Execute(AScript, AOptions, AParameters: String); Overload;
-    procedure Execute(AScript: String); Overload;
+    function Execute(AScript, AOptions, AParameters: String): Boolean; Overload;
+    function Execute(AScript: String): Boolean; Overload;
     class var Instance: TAHKManager;
+
+    property Variables[Name: String]: String read GetVariable write SetVariable;
   end;
 
 implementation
@@ -31,18 +41,46 @@ begin
   FHandle:=LoadLibrary('AutoHotkey.dll');
   if FHandle <> 0 then
   begin
-    @FTextDll:=GetProcAddress(FHandle, 'ahktextdll');
+    @FThread:=GetProcAddress(FHandle, 'ahkTextDll');
+    @FThreadReady:=GetProcAddress(FHandle, 'ahkReady');
+    @FExecute:=GetProcAddress(FHandle, 'ahkExec');
+    @FGetVariable:=GetProcAddress(FHandle, 'ahkGetVar');
   end;
 end;
 
-procedure TAHKManager.Execute(AScript, AOptions, AParameters: String);
+function TAHKManager.Execute(AScript, AOptions, AParameters: String): Boolean;
+var
+  Handle: THandle;
+  X: Integer;
 begin
-  FTextDll(PWideString(AScript), PWideString(AOptions), PWideString(AParameters));
+  Handle:=0;
+  Handle:=FThread(nil, nil, nil);
+  if Handle <> 0 then
+  begin
+    if FThreadReady <> 0 then
+    begin
+      Result:=FExecute(PWideString(AScript));
+    end;
+  end;
 end;
 
-procedure TAHKManager.Execute(AScript: String);
+function TAHKManager.Execute(AScript: String): Boolean;
 begin
   Execute(AScript, '', '');
+end;
+
+function TAHKManager.GetVariable(Name: String): String;
+var
+  Variable: PWideString;
+begin
+  Variable:=FGetVariable(PWideString(Name), 0);
+
+  Result:=Copy(String(Variable), 0, Pos(#0, String(Variable))-1);
+end;
+
+procedure TAHKManager.SetVariable(Name: String; const Value: String);
+begin
+
 end;
 
 initialization
