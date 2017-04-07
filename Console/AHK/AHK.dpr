a9ffb54eff60d66b2f3054a06b3b56dc75295ddb
David Dembinski <darkvplayer@gmail.com>
2017-04-07 13:38:42 +0200

    Commit:   + Updated AHK.dpr

--- Console/AHK/AHK.dpr
+++ Console/AHK/AHK.dpr
@@ -8,9 +8,21 @@ uses
   System.SysUtils,
   AHKManager in 'Source\AHKManager.pas';
 
+var
+  I:Integer;
+  Data: String;
+
 begin
   try
-    TAHKManager.Instance.Execute('Msgbox Hello World');
+
+    for I := 0 to 100 do
+    begin
+      TAHKManager.Instance.Execute('Dick:=10 + 5');
+      Data:=TAHKManager.Instance.Variables['Dick'];
+
+      WriteLn(Data);
+    end;
+
     Readln;
   except
     on E: Exception do
